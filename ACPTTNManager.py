b74b3f3e9122a5df3443ccc853adbf90aff208d0
rv355 <rv355@cam.ac.uk>
2020-10-12 17:21:48 +0100

    Migrated from Python SDK to HTTP Application Manager API

--- ACPTTNManager.py
+++ ACPTTNManager.py
@@ -1,96 +1,164 @@
 import config
-import ttn
-import binascii
+import json
+import requests
+
+# Class used to define a TTN Application client.
+class Client:
+    url = ''
+    headers = ''
+
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+
 
 class ACPTTNManager:
-    ttn_handler = ttn.HandlerClient(config.DEFAULT_APP_ID, config.DEFAULT_ACCESS_KEY)
-    app_client =  ttn_handler.application()
+    client = Client(
+                        config.URL+config.DEFAULT_APP_ID, 
+                        {"Authorization": "Key "+config.DEFAULT_ACCESS_KEY, "Content-Type":"application/json"}
+                )
 
     @classmethod
     def get_app_details(self):
         '''
         Return the TTN Application object
         '''
-        my_app = self.app_client.get()
-        return(my_app)
+        response = requests.get(self.client.url,headers=self.client.headers)
+        return response.json()
 
     @classmethod
     def get_all_devices(self, client=None):
         '''
         Return all devices registered on the specified client application
-        client: The application client. Use the default application if None
+        client: The application client. Use the default application client if None
         '''
         if client == None:
-            client = self.app_client
-        my_devices = client.devices()
-        return(my_devices)
+            client = self.client
+        response = requests.get(client.url+"/devices",headers=client.headers)
+
+        return response.json()
+        
 
     @classmethod
     def get_device_details(self, device_id, client=None):
         '''
         Return the device object corresponding to 'device_id' on the specified client application
-        client: The application client. Use the default application if None
+        client: The application client. Use the default application client if None
         '''
         if client == None:
-            client = self.app_client
-        return client.device(device_id)
+            client = self.client
+        response = requests.get(client.url+"/devices/"+device_id,headers=client.headers)
+
+        return response.json()
 
     @classmethod
     def register_new_devices(self, devices):
         '''
         Register all devices contained in the list 'devices'
-        devices: A list of device dictionaries of the form {'dev_id':dev_id, 'dev_details':{A dictionary of all device details}}
+        devices: A list of device dictionaries of the form defined at https://www.thethingsnetwork.org/docs/applications/manager/api.html
         '''
+        response_list = []
         for device in devices:
-            self.app_client.register_device(device['dev_id'], device['dev_details'])
+            response = requests.post(self.client.url+"/devices",headers=self.client.headers, data=json.dumps(device))
+            if response.status_code != 200:
+                response_list.append(device['dev_id']+':'+response.text)
+        if len(response_list) == 0:
+            return 'All devices added'
+        else:
+            return json.dumps(response_list)
+        
 
     @classmethod
-    def delete_devices(self, dev_ids=None, client=None):
+    def delete_device(self, dev_id, client=None):
         '''
-        Delete all devices whose dev_ids are in 'dev_ids'.
-        dev_ids: List containing dev_id of the devices to be deleted. Delete all devices if None
-        client: The TTN application client from which the devices should be removed. Default: Default TTN Application of the class
+        Delete a device with device id as dev_id.
+        dev_id: Device id of the device to be deleted
+        client: The TTN application client from which the devices should be removed. Use the default application client if None
         '''
         if client == None:
-            client = self.app_client
+            client = self.client
 
-        if dev_ids == None:
-            dev_ids = []
-            all_devices = self.get_all_devices(client)
-            for dev in all_devices:
-                dev_ids.append(dev.dev_id)
+        response = requests.delete(client.url+"/devices/"+dev_id, headers=client.headers)
 
-        for dev_id in dev_ids:
-            client.delete_device(dev_id)
+        return response.json()
+    
+    
 
     @classmethod
-    def migrate_devices(self, from_app_id, from_access_key, migration_key, dev_ids=None):
+    def migrate_devices(self, dev_ids=None):
         '''
         Migrate a set of devices from a TTN application to the default application of the class
         dev_ids: List containing the dev_id of all the devices to be migrated. Migrate all devices if empty
-        from_app_id: Application Id of the application from which the devices are to be migrated
-        from_access_key: Access key of the application from which the devices are to be migrated
-        migration_key: App Key to be used for registering the device on the default application
         '''
-        from_ttn_handler = ttn.HandlerClient(from_app_id, from_access_key)
-        from_app_client = from_ttn_handler.application()
+        migrate_client = Client(
+                                    url="https://eu.thethings.network:8094/applications/"+config.MIGRATE_APP_ID,
+                                    headers={"Authorization": "Key "+config.MIGRATE_ACCESS_KEY, "Content-Type":"application/json"}
+                            )
+        response_list = []
 
-        if dev_ids == None:
-            dev_ids = []
-            all_devices = self.get_all_devices(from_app_client)
-            for dev in all_devices:
-                dev_ids.append(dev.dev_id)
-        devices = []
-        for dev_id in dev_ids:
-            device = self.get_device_details(dev_id, from_app_client)
-            devices.append({
-                "dev_id" : dev_id, 
-                "dev_details" : {
-                    "appEui" : config.DEFAULT_APP_EUI,
-                    "devEui" : binascii.b2a_hex(device.lorawan_device.dev_eui).upper(),
-                    "appKey" : migration_key    
-                }})
+        if dev_ids == None:            
+            devices = self.get_all_devices(migrate_client)
+            for device in devices['devices']:
+                response = ACPTTNManager.__migrate_device(device, self, migrate_client)
 
-        self.delete_devices(dev_ids, from_app_client)
+                if response.status_code != 200:
+                    response_list.append(device['dev_id']+':'+response.text)
+
+        else:
+            for dev_id in dev_ids:
+                device = self.get_device_details(dev_id, migrate_client)
         
-        self.register_new_devices(devices)
+                response = ACPTTNManager.__migrate_device(device, self, migrate_client)
+
+                if response.status_code != 200:
+                    response_list.append(device['dev_id']+':'+response.text)
+
+        if len(response_list) == 0:
+            return "All devices migrated"
+        else:
+            return json.dumps(response_list)
+
+    #Create a device dictionary
+    def get_new_device(dev_eui, dev_id):
+        migrate_device = {
+                            "altitude": 0,
+                            "app_id": config.DEFAULT_APP_ID,
+                            "attributes": {
+                                "key": "",
+                                "value": ""
+                            },
+                            "description": "Some description of the device",
+                            "dev_id": dev_id,
+                            "latitude": 0.0,
+                            "longitude": 0.0,
+                            "lorawan_device": {
+                                "activation_constraints": "local",
+                                "app_eui": config.DEFAULT_APP_EUI,
+                                "app_id": config.DEFAULT_APP_ID,
+                                "app_key": config.MIGRATION_APP_KEY,
+                                "dev_eui": dev_eui,
+                                "dev_id": dev_id,
+                                "disable_f_cnt_check": False,
+                                "f_cnt_down": 0,
+                                "f_cnt_up": 0,
+                                "last_seen": 0,
+                                "uses32_bit_f_cnt": True
+                            }
+                        }
+
+        return migrate_device
+
+    # Migrate a device from the migrate client to the default client
+    @staticmethod
+    def __migrate_device(device, self, migrate_client):
+        dev_eui = device['lorawan_device']['dev_eui']
+        dev_id = device['dev_id']
+
+        self.delete_device(dev_id,migrate_client)
+
+        new_device = self.get_new_device(dev_eui, dev_id)
+
+        response = requests.post(self.client.url+"/devices",headers=self.client.headers, data=json.dumps(new_device))
+        
+        return response
+
--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Adaptive City Program TTN Manager
-(More testing needed)
 ACP TTN Manager provides means to manage TTN application and devices. It supports the following functionalities;
 1. Get details of the TTN Application
 2. Get details of all the registered devices on any application
 3. Get details of a specific device on any application
 4. Register a set of new devices to the default application
-5. Delete a set of devices from any application
+5. Delete a device from any application
 6. Migrate a set of devices from any application to the default application
 
 ## Installation
@@ -28,14 +27,13 @@ python3 -m pip install -r requirements.txt
 Create a `config.py` file with the following entries;
 
 ```
+URL = "https://eu.thethings.network:8094/applications/"
+
 DEFAULT_APP_ID = ""
 DEFAULT_ACCESS_KEY = ""
 DEFAULT_APP_EUI = ""
 
-# Details of application from which migrating
-MIGRATE_APP_ID = ""
-MIGRATE_ACCESS_KEY = ""
-MIGRATION_APP_KEY = ""
+# Details of application from which migratingmanager.migrate_devices()
 ```
 
 Import the ACPTTNManager class and use the following class methods for the required functionalities
@@ -43,8 +41,8 @@ Import the ACPTTNManager class and use the following class methods for the requi
 1. `get_app_details()`: Return the TTN Application object
 2. `get_all_devices(client)`: Return all devices registered on the specified client application. Uses default client if no client specified.
 3. `get_device_details(device_id, client)`: Return the device object corresponding to 'device_id' on the specified client application. Uses default client if no client specified.
-4. `register_new_devices(devices)`: Register all devices contained in the list 'devices'. All list elements should be dictionaries of the format `{'dev_id':dev_id, 'dev_details':{A dictionary of all device details. Must contain "appEui", "devEui", "appKey"}}`. All device details keys available at *https://www.thethingsnetwork.org/docs/applications/python/api-reference.html*
-5. `delete_devices(dev_ids, client)`: Delete all devices whose dev_id are in 'dev_ids'.  Uses default client if no client specified. Delete all devices if no dev_ids specified.
-6. `migrate_devices(from_app_id, from_access_key, migration_key, dev_ids)`: Migrate a set of devices from a TTN application to the default application of the class. Migrate all devices if no dev_ids specified.
+4. `register_new_devices(devices)`: Register all devices contained in the list 'devices'. All list elements should be dictionaries of the format defined at *https://www.thethingsnetwork.org/docs/applications/manager/api.html*
+5. `delete_device(dev_id, client)`: Delete all devices whose dev_id are in 'dev_ids'.  Uses default client if no client specified. Delete all devices if no dev_ids specified.
+6. `migrate_devices(dev_ids)`: Migrate a set of devices from a TTN application to the default application of the class. Migrate all devices if no dev_ids specified.
 
 See the `test.py` file for examples.
