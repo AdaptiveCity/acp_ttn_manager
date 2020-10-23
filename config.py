5ed2ef34639d7dbb2aa2c9bff2c311a8bfe88ec9
rv355 <rv355@cam.ac.uk>
2020-10-23 14:02:48 +0100

    Included config file - changed to private repo

---
+++ config.py
@@ -0,0 +1,16 @@
+# DEFAULT_APP_ID = "vlab-sensor-network-2"
+# DEFAULT_ACCESS_KEY = "ttn-account-v2.GF1V1cBixTRtNDLeq_LL2vgsL7Rt0fAOdRtp9RMRSxY"
+
+# MIGRATE_APP_ID = "cambridge-sensor-network"
+# MIGRATE_ACCESS_KEY = "ttn-account-v2.NmK5TOZQqsa6LhSHFwu2CXcTytBsVjRe0OVn7Ua1iQc"
+
+URL = "https://eu.thethings.network:8094/applications/"
+
+MIGRATION_APP_KEY = "22C905D1A45D1EAED8D950B915ECC8D8"
+
+DEFAULT_APP_ID = "vlab-sensor-network"
+DEFAULT_ACCESS_KEY = "ttn-account-v2.qB7r40lWa1hA-S8Y2gHmNII6Tg7sFK1m81rubx1_OIM"
+DEFAULT_APP_EUI = "70B3D57ED00358FF"
+
+MIGRATE_APP_ID = "vlab-sensor-network-2"
+MIGRATE_ACCESS_KEY = "ttn-account-v2.GF1V1cBixTRtNDLeq_LL2vgsL7Rt0fAOdRtp9RMRSxY"
