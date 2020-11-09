6282637bdea5853828bd421b6dd1d77daab4c43b
rv355 <rv355@cam.ac.uk>
2020-11-09 16:47:56 +0000

    examples updated

--- test.py
+++ test.py
@@ -11,8 +11,7 @@ def load_settings():
 
 manager = ACPTTNManager(load_settings(), 'vlab-sensor-network-2')
 
-# app = manager.get_app_details()
-# devices = manager.get_all_devices()
-device = manager.get_device_details("elsys-co2-041abc")
-print(device.keys())
-# manager.migrate_devices('vlab-sensor-network-2')
+app = manager.get_app_details()
+devices = manager.get_all_devices()
+device = manager.get_device_details("elsys-co2-041bab")
+manager.migrate_devices('vlab-sensor-network-2')
