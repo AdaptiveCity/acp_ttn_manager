2e1b6f55430c264ed888284ad700827eaf8392ae
rv355 <rv355@cam.ac.uk>
2021-03-09 18:04:32 +0000

    device registration test added in test.py

--- test.py
+++ test.py
@@ -1,53 +1,23 @@
 from ACPTTNManager import ACPTTNManager
 import json
-import sys
+from datetime import datetime
 
 def load_settings():
-    with open('secrets/settings_v3.json', 'r') as settings_file:
+    with open('secrets/settings.json', 'r') as settings_file:
         settings_data = settings_file.read()
 
     # parse file
     settings = json.loads(settings_data)
     return settings
 
-# manager = ACPTTNManager(load_settings(), 'vlab-sensor-network-2')
-
-# app = manager.get_app_details()
-# devices = manager.get_all_devices()
-# device = manager.get_device_details("elsys-co2-041bab")
-# manager.migrate_devices('vlab-sensor-network-2')
-
-#manager = ACPTTNManagerV3(load_settings(), 'cambridgesensornetwork', '3')
-manager = ACPTTNManager(load_settings(), 'vlab-sensor-network')
+manager = ACPTTNManager(load_settings(), 'cambridge-net-3')
 
 app = manager.get_app_details()
 devices = manager.get_all_devices()
-# print(app)
-print(devices)
-# device = manager.get_device_details("elsys-co2-0520a7")
-# device = manager.get_device_details("elsys-co2-041bab")
-# print(device)
-# manager.delete_device('test-device')
-# manager.migrate_devices('vlab-sensor-network-2')
-
-def write(manager, json_file, device_id=None):
-    with open(json_file) as jfile:
-        json_data = json.load(jfile)
-        # We will track whether the ttn app was updated
-        app_updated=False
-        for json_id in json_data.keys():
-            if device_id is None or device_id==json_id:
-                app_updated=True
-                ttn_settings = json_data[json_id]['ttn_settings'] 
-                print(json_id,': ',manager.register_device(ttn_settings))
-        if not app_updated:
-            if device_id:
-                print(f"TTN application not updated (--id {device_id} not found?)", file=sys.stderr)
-            else:
-                print("Warning: TTN application not updated.", file=sys.stderr)
-
-# write(manager, 'device_v3.json')
-
-# print(manager.delete_device('elsys-co2-045abc'))
-
-# manager.migrate_devices('vlab-sensor-network-2')               
+            
+for device in devices['end_devices']:
+    updated_time = device['updated_at'][:-4]
+    dt = datetime.strptime(updated_time, '%Y-%m-%dT%H:%M:%S.%f')
+    last_seen = ((datetime.now() - dt).seconds)/3600
+    if last_seen > 2.0:
+        print(device['ids']['device_id'])
